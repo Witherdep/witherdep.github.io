1e963ee963098351207e14d214776b5d37e50c14
Witherdep <witherpedbr@gmail.com>
2024-09-24 14:28:39 -0300

    Add files via upload

---
+++ rodape.php
@@ -0,0 +1,5 @@
+<footer>
+    <div id="footer_central">
+    <p>Café do Djabo é uma empresa fictícia e sem relações com satanismo.</p>
+    </div>
+</footer>
---
+++ topo.php
@@ -0,0 +1,11 @@
+<header style="display: flex">
+    <div id="header_central" style="padding-left: 200px;" >
+        <a href="listagem.php" style="text-decoration: none;" >
+            <img style="width: auto;height:130px;" src="/projetos/public/images/logo sem fundo alt tracado.png">
+            <img style="width: auto;height:40px; margin:  25px;    margin-bottom: 55px;" src="/projetos/public/images/frase.png">
+        </a>
+    </div>
+    <nav id="navegacao" style=" margin: 5px; ">
+        <a style="text-decoration: none;color: #c95389;" href="login.php"><div id="botao_login">Fazer Login</div></a>
+    </nav>
+</header>
